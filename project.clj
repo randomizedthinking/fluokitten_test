74e64d70473b42ca7fa2ef6e925a080659fdf6ec
Gang Liang <randomizedthinking@gmail.com>
2019-01-02 11:08:44 -0800

    first commit

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-				 [uncomplicate/neanderthal "0.20.4"]
+                 [uncomplicate/neanderthal "0.20.4"]
                  [uncomplicate/fluokitten "0.9.0"]
                  [criterium "0.4.4"]]
   :main fluokitten-test.core
