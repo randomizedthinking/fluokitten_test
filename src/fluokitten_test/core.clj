970213a8dfecd919ef9f0f8b28d02d9b62d9b92e
Gang Liang <randomizedthinking@gmail.com>
2019-01-03 00:23:20 -0800

    add *unchecked-math* globally

--- src/fluokitten_test/core.clj
+++ src/fluokitten_test/core.clj
@@ -6,10 +6,16 @@
 (def n 100000)
 (def nx (dv (range n)))
 (def ny (dv (range n)))
+(def cvx (range n))
+(def cvy (range n))
 (defn p+ ^double [^double x ^double y] (+ x y))
 (defn p* ^double [^double x ^double y] (* x y))
 
 (defn -main
   []
-  (with-progress-reporting (quick-bench (foldmap p+ 0.0 p* nx ny)))
+  (println "(quick-bench (foldmap p+ 0.0 p* nx ny))")
+  (quick-bench (foldmap p+ 0.0 p* nx ny))
+
+  (println "(quick-bench (foldmap p+ 0.0 p* cvx cvy))")
+  (quick-bench (foldmap p+ 0.0 p* cvx cvy))
   )
